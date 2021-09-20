dd60411b7b46ebc160ea2f61878cac5378449f33
dohyun93 <kwondohyun93@naver.com>
2021-09-21 08:19:59 +0900

    Chapter5. fmt I/o

--- chapter5_fmt_IO/정수실수 자리수 출력연습/5.2.go
+++ chapter5_fmt_IO/정수실수 자리수 출력연습/5.2.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var myAge int64 = 29
 	var yourAge int64 = 35
-	var myHeight float64 = 7.22
+	var myHeight float64 = 3.1415
 	var myFloat float64 = 234.12349
 	// 1. 정수와 실수의 출력
 	// %d, %f -> 정수와 실수 출력. fmt.Printf 함수로 사용. %f == %.6f
@@ -18,6 +18,6 @@ func main() {
 
 	fmt.Printf("%05d, %05d\n", myAge, yourAge) // 최소 표시되는 자리수가 5인 정수를 출력. 5보다 짧은 길이의 정수면 앞에 0을 채움.
 	fmt.Printf("%-5d, %-3d\n", myAge, yourAge) // 최소 표시되는 자리수가 5, 3인 정수를 출력. 0옵션을 줘도 적용되지 않음.
-	fmt.Printf("%5.3f\n", myHeight)            // 최소 너비 5. 소수점 이하 3자리.
+	fmt.Printf("%05.2f\n", myHeight)           // 최소 너비 5. 소수점 이하 3자리.
 	fmt.Printf("%7.2g\n", myFloat)             // 최소 너비 7. 총 숫자 2자리.
 }
