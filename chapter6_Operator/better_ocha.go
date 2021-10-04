a952cc20c7fe549dc014f1cf98cd6ba9141ef344
dohyun93 <kwondohyun93@naver.com>
2021-10-04 15:46:44 +0900

    부동 소수점 학습했다.

--- chapter6_Operator/better_ocha.go
+++ chapter6_Operator/better_ocha.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/big"
 	"reflect"
 )
 
+func equal(a, b float64) bool {
+	return math.Nextafter(a, b) == b
+	// 만약 a가 b보다 작다면 a보다 1bit큰 값을 반환. 이게 b와 같다면 1bit차이로 작은 a가 b와 같다고 간주한다.
+	// 만약 a가 b보다 크다면 a보다 1bit작은 값을 반환. 이게 b와 같다면 1bit차이로 큰 a가 b와 같다고 간주한다.
+	// Nextafter함수는 a와 b가 같다면 a를 반환.
+	// 그 이상의 차이는 두 수가 다르다고 본다.
+	// 즉, 0~1bit차이만 허용한 비교법임.
+}
+
 func main() {
+	// *** 1bit라는 최소한의 오차를 허용하는 방법 *** //
+	// 1. Nextafter(a, b float64) (ret float64)
+	// float64 형 변수 a, b를 입력받아, a가 b보다 작다면 a를 1bit만큼 증가시키고, 그렇지 않다면 a보다 1bit작은 값 반환.
+
+	var var1, var2, var3 float64 = 0.1, 0.2, 0.3
+
+	fmt.Printf("%0.18f + %0.18f = %0.18f\n", var1, var2, var1+var2)
+	fmt.Printf("%0.18f == %0.18f : %v\n", var3, var1+var2, equal(var3, var1+var2))
+
+	// *** math/big 패키지를 활용해 Float 객체로 더욱 정밀한 실수 계산하기 *** //
 	a, _ := new(big.Float).SetString("0.1")
 	b, _ := new(big.Float).SetString("0.2")
 	c, _ := new(big.Float).SetString("0.3")
 
 	d := new(big.Float).Add(a, b)
 	fmt.Println(a, b, c, d)
-	fmt.Println(c.Cmp(d)) // -1은 c < d, 1은  c > d.
-
+	switch c.Cmp(d) {
+	case 0:
+		fmt.Println("c.Cmp(d) 결과는 0. c == d 정밀계산 완료")
+	case -1:
+		fmt.Println("c.Cmp(d) 결과는 -1. c < d 정밀 계산 완료")
+	case 1:
+		fmt.Println("c.Cmp(d) 결과는 1. c > d 정밀 계산 완료")
+	}
 	fmt.Println(reflect.TypeOf(a))
 
 	// [출력]
