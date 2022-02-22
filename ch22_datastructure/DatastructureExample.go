00fdef2a0f3203fd18a1a835fa3d7e364d58443c
dohyun93 <kwondohyun93@naver.com>
2022-02-22 22:31:37 +0900

    2022년 2월 22일 chapter22. data structure

--- ch22_datastructure/DatastructureExample.go
+++ ch22_datastructure/DatastructureExample.go
@@ -1,8 +1,13 @@
 package ch22_datastructure
 
+import (
+	"container/list"
+	"fmt"
+)
+
 func DataStructure() {
 	// 22장은 자료구조로, 아래 세 개의 자료구조에 대해 배운다.
-
+	fmt.Println()
 	// 1. 리스트
 	// 2. 링
 	// 3. 맵
@@ -20,7 +25,17 @@ type Element struct {
 func List() {
 	// list는 기본 자료구조로 여러 데이터를 보관할 수 있다.
 	// 배열은 연속된 메모리에 저장하지만, 리스트는 불연속된 메모리 공간에 저장한다는 차이가 있다.
+	myList := list.New()
+	element4 := myList.PushBack(4)
+	element1 := myList.PushFront(1)
+	myList.InsertBefore(3, element4)
+	myList.InsertAfter(2, element1)
+	// 1 2 3 4
 
+	for e := myList.Front(); e != nil; e = e.Next() {
+		// Next: 다음 요소의 인스턴스를 가리킴.
+		fmt.Print(e.Value, " ")
+	}
 	// 1. 포인터로 연결된 요소
 	// 리스트는 각 데이터를 담고있는 요소들을 포인터로 연결한 자료구조다.
 	// 요소들이 서로 포인터로 연결되었다고 해서 linked list라고 부르기도 한다.
