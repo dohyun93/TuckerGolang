f4275534a2dba957e89bab73903194fa1e9868d9
dohyun93 <kwondohyun93@naver.com>
2022-03-26 21:38:48 +0900

    2022년 3월 21일 chapter25. channel and context
    channel: 고루틴 간 메시지를 전달하는 메시지 큐.
    context: 고루틴에 작업 요청 시 작업 시간, 취소, 추가 데이터 등을 지정할 수 있는 일종의 작업 명세서 역할을 함.

--- ch25_channer_context/1. Channel Example.go
+++ ch25_channer_context/1. Channel Example.go
@@ -15,14 +15,14 @@ func ChannelExample() {
 	go square(&wg, ch)
 	//고루틴 생성. 채널 인스턴스를 받아서 채널에서 데이터를 받아오려고 시도하지만, 아직 채널에 데이터가 없기 때문에
 	// 데이터가 들어올 때 까지 대기한다.
+	fmt.Println("5초간 잠듬..")
+	time.Sleep(time.Second * 5)
 	ch <- 9   // 이제 데이터를 넣어줬기 때문에, 22행에서 변수 n으로 채널의 데이터를 빼올 수 있다. 26행에서 대기중인 고루틴의 개수 -1.
 	wg.Wait() // 모든 고루틴이 종료될 때 까지 대기..
 }
 
 func square(wg *sync.WaitGroup, ch chan int) {
 	n := <-ch // 채널 인스턴스 ch에 담긴 데이터를 꺼내 n 변수에 담는다..
-
-	time.Sleep(time.Second)
-	fmt.Printf("Square : %d\n", n*n)
+	fmt.Printf("값 전달됨. Square : %d\n", n*n)
 	wg.Done() // 대기중인 고루틴의 수행이 종료됨을 알려준다.
 }
