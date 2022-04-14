d30dc93801e90ef2569b6239f5a2a852fa5616d5
dohyun93 <kwondohyun93@naver.com>
2022-04-15 08:21:41 +0900

    2022년 4월 3일 TuckerGolang - 1~2권 recap중.
    18장. slice

--- ch18_slice/slice_recap.go
+++ ch18_slice/slice_recap.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func SliceRecap() {
 	// var runeSlice = []rune{'k', 'w', 'o', 'n'}
-	runeSlice2 := make([]rune, 10, 9)
+	runeSlice2 := make([]rune, 10, 10)
 	for idx, val := range runeSlice2 {
 		fmt.Printf("%d, %c\n", idx, val)
 	}
+	fmt.Println("runeSlice2 length: len(runeSlice2)", len(runeSlice2))
+	myString := "hello"
+
+	// len(string): 크기
+	// len(slice/array): 길이
+
+	fmt.Println("myString length: len(myString)", len(myString))
+	myString2 := "hello권"
+	fmt.Println("myString2 length: len(myString2)", len(myString2))
 }
