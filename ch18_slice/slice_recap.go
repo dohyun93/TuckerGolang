a6d8640408f8861f8b5c1cf547650f399428da58
dohyun93 <kwondohyun93@naver.com>
2022-04-14 23:09:03 +0900

    2022년 4월 3일 TuckerGolang - 1~2권 recap중.
    18장. slice

--- ch18_slice/slice_recap.go
+++ ch18_slice/slice_recap.go
@@ -3,30 +3,9 @@ package ch18_slice
 import "fmt"
 
 func SliceRecap() {
-	var myRunes = []rune{'가', '나', '다'}
-	var myString = ""
-
-	fmt.Println("myRunes: ", myRunes)
-	for _, val := range myRunes {
-		fmt.Printf("%c ", val)
+	// var runeSlice = []rune{'k', 'w', 'o', 'n'}
+	runeSlice2 := make([]rune, 10, 9)
+	for idx, val := range runeSlice2 {
+		fmt.Printf("%d, %c\n", idx, val)
 	}
-
-	myString = string(myRunes) /////////////////////
-	fmt.Println("\nstringed myRunes: ", myString)
-
-	// 기본값 0으로 출력됨.
-	var makeSlice = make([]int, 3)
-	for _, val := range makeSlice {
-		fmt.Printf("made by make keyword slice: %d\n", val)
-	}
-
-	fmt.Println(len(makeSlice), cap(makeSlice))
-
-	makeSlice = append(makeSlice, 4)
-	fmt.Println(makeSlice)
-
-	fmt.Println(len(makeSlice), cap(makeSlice))
-
-	Slice1 := make([]int, 3, 5) // type, len, cap
-	fmt.Println(len(Slice1), cap(Slice1))
 }
